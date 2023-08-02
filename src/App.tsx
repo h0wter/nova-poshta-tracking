01af4d4c55e4aa9d61dbe8184308ac768609d945
h0wter <91832203+h0wter@users.noreply.github.com>
2023-08-02 22:50:00 +0300

    npt-1: + routing into app and main components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { Global } from "@emotion/react";
 import { GlobalStyles } from "./GlobalStyles";
+import { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout";
+
+const MainPage = lazy(() => import("./pages/MainPage"));
+const OfficesPage = lazy(() => import("./pages/OfficesPage"));
 
 function App() {
   return (
     <>
-      <h1>Tracking</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="/offices" element={<OfficesPage />} />
+        </Route>
+      </Routes>
       <Global styles={GlobalStyles} />
     </>
   );
